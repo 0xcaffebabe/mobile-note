d50808085ec2c7a7dde5418d7b3f2fe7a47531e4
MY <715711877@qq.com>
2022-04-14 20:15:57 +0800

    Initial Commit

---
+++ MobileNote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MobileNote
+//
+//  Created by mymac on 2022/4/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MobileNote/MobileNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileNoteApp.swift
+//  MobileNote
+//
+//  Created by mymac on 2022/4/14.
+//
+
+import SwiftUI
+
+@main
+struct MobileNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
